10da8880b746637d4142cb4be7188456e0a96b74
jhonduarte <jduarte890@unab.edu.co>
2016-10-28 16:26:48 -0500

    Update demo_control.py

--- examples/demo_control.py
+++ examples/demo_control.py
@@ -17,20 +17,21 @@ class BlobAnalysis:
             ChullArea = list()
             contourArea = list()
             
-            while cs: 
-            if asb(cv.ContourArea(cs)) > 2000 #Filtra contornos pequeños de mas de 2000 pixeles en el área
-                ContourArea.append(cv.ContourArea(cs)) #Añade ContourArea con el nuevo contorno
-                  try:
-                        m10 = int(cv.GetSpatialMoment(m,1,0))
-                        m00 = int(cv.GetSpatialMoment(m,0,0))
-                        m01 = int(cv.GetSpatialMoment(m,0,1))
-                        centroid.append(cv.Convexhull2(cs,cv.CreateMemStorage(),return_points=True) #Busca el convezo de cd en CvSeq                    convexHull = cv.ConvexHull2(cs,cv.CreateMemStorage(),return_points=True) #Agrega el formulario de lista ConvexHull a la lista cHull             cHullArea.append(cv.ContourArea(convexHull)) #Agrega el formulario de lista de la envolvente convexa a la lista cHull
-                        cHull.append(list(convexHull)) #Agrega el formulario de lista del contorno a la lista de contornos
-                        contours.append(list(cs)) 
-                        counter += 1 #Añade al counter para ver cuantos pixeles están all
-                  except:
-                         pass
-           cs = cs.h_next()
+            while cs:
+                  if asb(cv.ContourArea(cs)) > 2000 #Filtra contornos pequeños de mas de 2000 pixeles en el área
+                        ContourArea.append(cv.ContourArea(cs)) #Añade ContourArea con el nuevo contorno
+                        m = cv.Moments(cs)
+                        try:
+                              m10 = int(cv.GetSpatialMoment(m,1,0))
+                              m00 = int(cv.GetSpatialMoment(m,0,0))
+                              m01 = int(cv.GetSpatialMoment(m,0,1))
+                              centroid.append(cv.Convexhull2(cs,cv.CreateMemStorage(),return_points=True) #Busca el convezo de cd en CvSeq                    convexHull = cv.ConvexHull2(cs,cv.CreateMemStorage(),return_points=True) #Agrega el formulario de lista ConvexHull a la lista cHull             cHullArea.append(cv.ContourArea(convexHull)) #Agrega el formulario de lista de la envolvente convexa a la lista cHull
+                              cHull.append(list(convexHull)) #Agrega el formulario de lista del contorno a la lista de contornos
+                              contours.append(list(cs)) 
+                              counter += 1 #Añade al counter para ver cuantos pixeles están all
+                        except:
+                              pass
+                  cs = cs.h_next()                      
         self.centroid = centroid
         self.counter = counter
         self.cHull = cHull
